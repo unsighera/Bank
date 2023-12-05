d1dadc844101f082dad8904469e047ff8dd1ed37
unsighera <eddy.weston@bk.ru>
2023-12-05 18:07:20 +0300

    Добавьте файлы проекта.

---
+++ Bank/Classes/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Classes
+{
+    public class BankAccount
+    {
+        decimal _balance = 0;
+        public decimal Balance { get { return _balance; } set { _balance = value; } }
+        protected string AccountNumber { get; set; }
+        private string _Password { get; set; }
+        public delegate void BalanceOperation(BankAccount account, int sum);
+
+        public BankAccount(string accountNumber, string password)
+        {
+            AccountNumber = accountNumber;
+            _Password = password;
+        }
+
+        public void Translation(BankAccount account, int sum)
+        {
+
+            if (_balance - sum >= 0)
+            {
+                account.Balance += _balance;
+                _balance -= sum;
+            }
+            else
+            {
+                Console.WriteLine("Not enough money, please top up your balance!");
+            }
+        }
+    }
+}
